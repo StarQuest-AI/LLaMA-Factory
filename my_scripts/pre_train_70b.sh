31f6d8173248370f858fe090de8691bab541eac1
nixingliang <nixingliang@shoppal.ai>
2024-03-25 13:54:12 +0000

    add train

---
+++ my_scripts/pre_train_70b.sh
@@ -0,0 +1,31 @@
+deepspeed --num_gpus 4 ../src/train_bash.py \
+    --deepspeed deepspeed_config_s3.json \
+    --ddp_timeout 180000000 \
+    --stage pt \
+    --do_train \
+    --model_name_or_path /data0/models/huggingface/meta-llama/Llama-2-70b-chat-hf \
+    --dataset movie_tv \
+    --dataset_dir ../movie_script \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj \
+    --output_dir ../saves/LLaMA2-70B-CHAT-MOVIE-TV/lora/pretrain \
+    --overwrite_cache \
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --warmup_steps 20 \
+    --save_steps 1000 \
+    --eval_steps 100 \
+    --evaluation_strategy steps \
+    --load_best_model_at_end \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --max_samples 10000 \
+    --val_size 0.1 \
+    --plot_loss \
+    --fp16
